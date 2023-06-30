f2b4afaed77a6077a9f87e436da02fecb89a3f66
«Konstantin <18089630@mail.ru>
2023-07-01 00:40:39 +0700

    com7

---
+++ DZ8/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Write ("Введите чисо N");
+int number = Convert.ToInt32(Console.ReadLine());
+int N = 2;
+if (number > 1)
+{
+          while (N <= number)
+{
+          Console.WriteLine(N + " ");
+          N = N + 2;
+}
+}
